f077be6bb13983fc1a1606e7cb3cac99aec231c6
Ben Lobaugh <ben@lobaugh.net>
2012-10-10 18:59:24 -0700

    Troubleshooting with Jaffe

--- js/front-page.js
+++ js/front-page.js
@@ -13,6 +13,11 @@ jQuery(function($){
         timeout: 0,
         pager: '.main-nav-menu'
     });
+    
+    $('.main-nav-menu').find('a:first-child').attr('title', 'whatever');
+    $('.main-nav-menu').find('a:nth-child(2)').attr('title', 'whatever2');
+    $('.main-nav-menu').find('a:nth-child(3)').attr('title', 'whatever3');
+    $('.main-nav-menu').find('a:nth-child(4)').attr('title', 'whatever4');
 
     $(".main-nav-menu a").tipTip();
     console.log($('main-nav-menu a'));
