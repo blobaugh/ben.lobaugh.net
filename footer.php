e82c21060ae2f12d435b77ed69fed89703f31b25
Ben Lobaugh <ben@lobaugh.net>
2012-10-10 19:23:55 -0700

    Seperating front-page code from rest of theme

--- footer.php
+++ footer.php
@@ -3,9 +3,3 @@
 
 
 
-	<div class="footer">
-        <p><!-- Contents &copy; Ben Lobaugh 1998-<?php echo date('Y', time());?> --></p>
-    </div>
-<script type='text/javascript' src='http://ben.lobaugh.net/wordpress/wp-content/themes/ben.lobaugh.net/js/front-page.js?ver=3.4.2'></script>
-</body>
-</html>
