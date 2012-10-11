108f2fe7242dbd8c270b279a786820802fb283b1
Ben Lobaugh <ben@lobaugh.net>
2012-10-10 18:29:26 -0700

    Updating

--- js/front-page.js
+++ js/front-page.js
@@ -7,16 +7,14 @@
 // Custom JS for Ben
 jQuery(function($){
 	
-	$('.content').cycle({
-		fx: 'scrollUp',
-		speed: 'fast',
-		timeout: 0,
-		pager: '.main-nav-menu'
-		});
+    $('.content').cycle({
+        fx: 'scrollUp',
+        speed: 'fast',
+        timeout: 0,
+        pager: '.main-nav-menu'
+    });
 
-
-       
-            $(".main-nav-menu a").tipTip();
+    $(".main-nav-menu a").tipTip();
        
 });
 
